e574a86ba363eb424e9e4a1b07bce6284f8203ff
Guillermo Velasco <guillermo.a.velasco@gmail.com>
2023-02-08 10:48:16 -0300

    Agregando Patron Factory Method

---
+++ Factory/class/Fabrica.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public abstract class Fabrica
+    {
+        public abstract ITransporte Construir();
+        public void mostrar()
+        {
+            ITransporte transporte = Construir();
+            transporte.mostrar();
+        }
+    }
+}
---
+++ Factory/class/Terrestre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class Terrestre : ITransporte
+    {
+        public void mostrar()
+        {
+            Console.WriteLine("Terrestre");
+        }
+    }
+}
