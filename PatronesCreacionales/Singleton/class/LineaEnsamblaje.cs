8dac5f47bd20cd3a4792b9869f7cc32f5916861e
Guillermo Velasco <guillermo.a.velasco@gmail.com>
2023-02-15 10:33:58 -0300

    Creacion de Patron Singleton

---
+++ PatronesCreacionales/Singleton/class/LineaEnsamblaje.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class LineaEnsamblaje
+    {
+        private LineaEnsamblaje() { }
+
+        private static LineaEnsamblaje _instancia;
+
+        private static readonly object _bloqueo = new object();
+
+        public static LineaEnsamblaje CrearInstancia(string nombre,Auto auto)
+        {
+            //verifica que no este creado
+            if (_instancia == null)
+            {
+                //bloquea
+                lock (_bloqueo)
+                {
+                    if (_instancia == null)
+                    {
+                       _instancia= new LineaEnsamblaje();
+                        Console.WriteLine("Auto Ingresado Linea Ensamblaje: "+nombre+" "+auto.ToString());
+                    }
+
+                }
+            }
+            return _instancia;
+        }
+    }
+}
