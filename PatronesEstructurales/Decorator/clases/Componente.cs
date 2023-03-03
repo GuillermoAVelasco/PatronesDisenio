dc5b1e65da13ab1b6118b7860fc851a2e993ee3c
Guillermo Velasco <guillermo.a.velasco@gmail.com>
2023-03-03 14:23:10 -0300

    Agregando Patron Decorator

---
+++ PatronesEstructurales/Decorator/clases/Componente.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Decorator
+{
+    public abstract class Componente
+    {
+        private string _nombre;
+
+        public Componente(string nombre) { _nombre = nombre; }
+
+        public string getNombre() { return _nombre; }
+        public void setNombre(string nombre) { _nombre=nombre; }
+
+        public bool EsCompuesto()
+        {
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return getNombre();
+        }
+
+    }
+}
